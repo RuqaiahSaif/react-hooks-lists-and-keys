ab9d71602d4ba980a6c9e4b38e7b8d1041d594d5
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-02-20 15:17:39 +0300

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -12,13 +12,14 @@ function ColorList() {
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
+      {colors.map((color,index)=>(
+        <ol>
+          <li key={index}style={{ color: color }}>{color}</li>
+      
+        </ol>
+      )
+      )}
+     
     </div>
   );
 }
